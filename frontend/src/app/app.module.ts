3a00d56673f36ebc1405af21d1f1d3a9f8a9ea73
Ti-malepe <malepe483@gmail.com>
2021-09-22 14:42:49 +0200

    Login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { LoginComponent } from './component/login/login.component';
@@ -9,7 +9,7 @@ import { RequestResetComponent } from './component/password/request-reset/reques
 import { ResponseResetComponent } from './component/password/response-reset/response-reset.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProfileComponent } from './component/profile/profile.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +22,9 @@ import { ProfileComponent } from './component/profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
